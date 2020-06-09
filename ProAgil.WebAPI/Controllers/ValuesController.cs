02c326abf3a555252198f875b83e2d718c2a752f
Lucas Silva Briguenti <lucas.briguenti@db1.com.br>
2020-06-09 13:50:41 -0300

    funcionando dbcontext

--- ProAgil.WebAPI/Controllers/ValuesController.cs
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
 
 namespace ProAgil.WebAPI.Controllers
@@ -11,21 +12,24 @@ namespace ProAgil.WebAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        public Evento[] eventos { get; set; }
+        public DataContext _contexto { get; }
+        public ValuesController(DataContext contexto)
+        {
+            _contexto = contexto;
+
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<Evento>> Get()
         {
-            Carregar();
-            return eventos;
+            return _contexto.Eventos.ToList();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<Evento> Get(int id)
         {
-            Carregar();
-            return eventos.FirstOrDefault(x => x.EventoId == id);
+            return _contexto.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
 
         // POST api/values
@@ -44,27 +48,6 @@ namespace ProAgil.WebAPI.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-        }
-        private void Carregar()
-        {
-            eventos =  eventos = new Evento[]{
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e .Net Core",
-                    Local = "Presidente Prudente",
-                    Lote = "1° Lote",
-                    QtdPessoas = 150,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e suas novidades",
-                    Local = "Presidente Prudente",
-                    Lote = "2° Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-            };
         }
     }
 }
